8acd0c193ab85015bbea1a152826cb023684dfd9
ppaul895 <ppaul895@gmail.com>
2022-07-19 04:03:47 -0400

    Various fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,25 @@
 // TOGGLE NAVIGATION MENU AND ICONS
 const menuToggleButton = document.querySelector('.menu-toggle-button');
 const menuElement = document.querySelector('.menu');
+const headerElement = document.getElementById('home');
 
 const toggleMenu = () => {
     menuElement.classList.toggle('active');
     menuToggleButton.classList.toggle('active');
+    if (headerElement.style.paddingBottom == "3.5rem") {
+        headerElement.style.paddingBottom = "0rem"
+    } else {
+        headerElement.style.paddingBottom = "3.5rem"
+    }
 }
 menuToggleButton.addEventListener('click', toggleMenu);
 
-// REMOVE ACTIVE CLASS FROM AND ICON ON LINK CLICK
+// REMOVE ACTIVE CLASS FROM ICON ON LINK CLICK
 const  removeActiveLinkClass = e => {
     if (e.target.classList.contains('list-link')) {
         menuElement.classList.remove('active');
         menuToggleButton.classList.remove('active');
+        headerElement.style.paddingBottom = "0rem"
     }
 }
 document.addEventListener('click', removeActiveLinkClass);
